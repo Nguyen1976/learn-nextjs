78081cfc79543f2a4daeb3ea23a6afbfd5686d03
Nguyen1976 <nguyen2202794@gmail.com>
2024-12-15 19:36:55 +0700

    Use fonts with next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  subsets: ["vietnamese"],
+  weight: ["100", "900"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <head></head>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
